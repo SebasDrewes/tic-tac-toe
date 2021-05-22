9e631afd821f5fc82a1e9646bccd3139ff33eeb7
Sebastian Drewes <sebas.drewes@gmail.com>
2021-05-21 22:11:46 -0300

    agregada funcionalidad pc random move

--- javascript.js
+++ javascript.js
@@ -6,8 +6,8 @@ const Player = (name, mark) => {
 
 const GameBoard = (() => {
     let gameBoard = ['','','','','','','','',''];
+    const display = document.querySelector(".display");
     const displayGameBoard = () => {
-        const display = document.querySelector(".display");
         while (display.firstChild) {
             display.removeChild(display.firstChild);
         }
@@ -32,14 +32,28 @@ const GameBoard = (() => {
             display.appendChild(cuadradito);
         }
     }
+
     const start = document.querySelector(".start");
     const restart = document.querySelector(".restart");
+    const send = document.querySelector(".send");
+    const opciones = document.querySelector(".opciones")
+    send.addEventListener("click", () => {
+        const firstName = document.createElement("input")
+        firstName.classList.add("firstName");
+        const secondName = document.createElement("input")
+        secondName.classList.add("secondName");
+        opciones.appendChild(firstName);
+        opciones.appendChild(secondName);
+    })
     start.addEventListener("click", () => {
         GameBoard.displayGameBoard();
         GamePlay.playerPlay();
-        start.disabled = true;
+        start.style.cssText = "display: none";
+        restart.style.cssText = "display: block";
     })    
     restart.addEventListener("click", () => {
+        const title = document.querySelector(".title");
+        title.textContent = "Tic-Tac-Toe";
         GameBoard.displayGameBoard();
         GamePlay.playerPlay();
         GamePlay.restart();
@@ -52,17 +66,29 @@ const GameBoard = (() => {
 const GamePlay = (() => {
     const firstPlayer = () => {
         const firstName = document.querySelector(".firstName")
+        if (firstName === null) {
+            const firstPlayer = Player("X player", "X");
+            return firstPlayer
+        } else {
         const firstPlayer = Player(firstName.value, "X");
         return firstPlayer;
+        }
     }
+
     const secondPlayer = () => {
         const secondName = document.querySelector(".secondName")
+        if (secondName === null) {
+            const secondPlayer = Player("O player", "O");
+            return secondPlayer
+        } else {
         const secondPlayer = Player(secondName.value, "O");
         return secondPlayer;
+        }
     }
 
     //funcion para alertar ganador
     const winner = () => {
+        const title = document.querySelector(".title");
         if ((GameBoard.gameBoard[0] === "X" && GameBoard.gameBoard[1] === "X" && GameBoard.gameBoard[2] === "X") || 
             (GameBoard.gameBoard[3] === "X" && GameBoard.gameBoard[4] === "X" && GameBoard.gameBoard[5] === "X") ||
             (GameBoard.gameBoard[3] === "X" && GameBoard.gameBoard[4] === "X" && GameBoard.gameBoard[5] === "X") ||
@@ -73,8 +99,8 @@ const GamePlay = (() => {
             (GameBoard.gameBoard[2] === "X" && GameBoard.gameBoard[5] === "X" && GameBoard.gameBoard[8] === "X") ||
             (GameBoard.gameBoard[0] === "X" && GameBoard.gameBoard[4] === "X" && GameBoard.gameBoard[8] === "X") ||
             (GameBoard.gameBoard[2] === "X" && GameBoard.gameBoard[4] === "X" && GameBoard.gameBoard[6] === "X")){
-                alert(`${firstPlayer().getName()} Won!`);
                 GameBoard.winDisplay();
+                title.textContent = `${firstPlayer().getName()} Won!`
             }else if((GameBoard.gameBoard[0] === "O" && GameBoard.gameBoard[1] === "O" && GameBoard.gameBoard[2] === "O") || 
             (GameBoard.gameBoard[3] === "O" && GameBoard.gameBoard[4] === "O" && GameBoard.gameBoard[5] === "O") ||
             (GameBoard.gameBoard[3] === "O" && GameBoard.gameBoard[4] === "O" && GameBoard.gameBoard[5] === "O") ||
@@ -85,13 +111,13 @@ const GamePlay = (() => {
             (GameBoard.gameBoard[2] === "O" && GameBoard.gameBoard[5] === "O" && GameBoard.gameBoard[8] === "O") ||
             (GameBoard.gameBoard[0] === "O" && GameBoard.gameBoard[4] === "O" && GameBoard.gameBoard[8] === "O") ||
             (GameBoard.gameBoard[2] === "O" && GameBoard.gameBoard[4] === "O" && GameBoard.gameBoard[6] === "O")){
-                alert(`${secondPlayer().getMark()} Won!`);
                 GameBoard.winDisplay();
+                title.textContent = `${secondPlayer().getName()} Won!`;
     }else if (GameBoard.gameBoard[0] !== "" && GameBoard.gameBoard[1] !== "" && GameBoard.gameBoard[2] !== ""
            && GameBoard.gameBoard[3] !== "" && GameBoard.gameBoard[4] !== "" && GameBoard.gameBoard[5] !== ""
            && GameBoard.gameBoard[6] !== "" && GameBoard.gameBoard[7] !== "" && GameBoard.gameBoard[8] !== ""){
-               alert("Empate!");
                GameBoard.winDisplay();
+               title.textContent = "Empate!";
         }}
     //funcion para tomar turnos y display mark en cuadradito
     let turn = true;
@@ -99,6 +125,38 @@ const GamePlay = (() => {
         const cuadradito = document.querySelectorAll(".cuadradito");
         for (let i = 0; i < cuadradito.length; i++) {
             let currentPlayer = firstPlayer();
+
+            //funcionalidad vs pc
+            const vspc = document.querySelector(".vspc")
+            if (vspc.checked === true) {
+                cuadradito[i].addEventListener('click', () => {
+                    if (turn === true && cuadradito[i].textContent === ""){
+                        currentPlayer = firstPlayer();
+                        cuadradito[i].textContent = currentPlayer.getMark()
+                        let data = cuadradito[i].getAttribute('data');
+                        GameBoard.gameBoard[data] = currentPlayer.getMark()
+        
+                        function generateRandom() {
+                            let randomNumbers = new Array();
+                            for(let i=0; i<8; i++)
+                            if(cuadradito[i].textContent === "")
+                            randomNumbers.push(i);
+                            let randomNumber = randomNumbers[Math.floor(Math.random() * randomNumbers.length)];
+                            return randomNumber;
+                        }
+
+                        let randomNumber = generateRandom();
+                        if (cuadradito[randomNumber] === undefined) {
+                            winner();
+                        } else {
+                        cuadradito[randomNumber].textContent = secondPlayer().getMark()
+                        let dataRandom = cuadradito[randomNumber].getAttribute('data');
+                        GameBoard.gameBoard[dataRandom] = secondPlayer().getMark();
+                        winner();
+                        }
+                    }
+            //funcionalidad vs player
+            })}else {
             cuadradito[i].addEventListener('click', () => {
                 if (turn === true && cuadradito[i].textContent === ""){
                     turn = false
@@ -116,7 +174,7 @@ const GamePlay = (() => {
                     GameBoard.gameBoard[data] = currentPlayer.getMark()
                     winner();
         };
-        })}}
+        })}}}
         const restart = () => {
             GameBoard.gameBoard = ['','','','','','','','',''];
             turn = true
@@ -126,5 +184,3 @@ const GamePlay = (() => {
             }}
     return {playerPlay, restart};
 })();
-
-
